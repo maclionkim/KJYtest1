ac088001ddc4bffbab5e95ffa438f91574daef34
dlmolnote <dlmolnote@DESKTOP-3M6UPJV>
2022-03-15 00:33:38 +0900

    asfasd

---
+++ src/Package/Testclass.java
@@ -0,0 +1,13 @@
+package Package;
+
+public class Testclass{
+	
+	public static void main(String[] args) {
+		System.out.println("hello!this is github practice");
+		//this is test github.	
+		//bbbb
+		//dsfsdfafsd
+		
+	}
+	
+}
